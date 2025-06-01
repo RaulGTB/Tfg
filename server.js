22352bce0d07e5d7411daf3b637c687062acaa42
Raul <raulgranadostbgmail.com>
2025-06-01 19:16:06 +0200

    Co-authored-by: Juan Pr0v3n <provenciozamorajuan@gmail.com>

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname, 'dist/esport-app')));
+app.use(express.static(path.join(__dirname, 'dist/browser/esport-app')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/esport-app/index.html'));
+  res.sendFile(path.join(__dirname, 'dist/esport-app/browser/index.html'));
 });
 
 app.listen(PORT, () => {
