20fbecefa3f2d24d14ba99632a35c51631eb2a92
Raul <raulgranadostbgmail.com>
2025-06-15 22:09:24 +0200

    Co-authored-by: Juan Pr0v3n <provenciozamorajuan@gmail.com>

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -6,12 +6,24 @@ import { Injectable } from '@angular/core';
 })
 export class DataService {
 
-  private urlBase = 'https://rjk-backend-hwb4bjfhexaybagg.spaincentral-01.azurewebsites.net/api/'
 
   constructor(private http: HttpClient) { }
 
   getLeagues() {
-    return this.http.get(this.urlBase + 'leagues')
+    return this.http.get('https://rjk-backend-hwb4bjfhexaybagg.spaincentral-01.azurewebsites.net/api/leagues')
+  }
+
+
+  getTeams() {
+    return this.http.get('https://api.pandascore.co/teams?token=rmhig-Fuz23S9tfDT14uoycApeyVynxJLv2Ljazjz3nYGTT7S4s')
+  }
+
+  getPlayers() {
+    return this.http.get('https://api.pandascore.co/players?token=rmhig-Fuz23S9tfDT14uoycApeyVynxJLv2Ljazjz3nYGTT7S4s')
+  }
+
+  getmatches() {
+    return this.http.get('https://api.pandascore.co/matches?token=rmhig-Fuz23S9tfDT14uoycApeyVynxJLv2Ljazjz3nYGTT7S4s')
   }
 
 }
