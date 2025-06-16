832d55250c9b51a0230cb71ed30182576249c14b
jproven <provenciozamorajuan@gmail.com>
2025-06-16 18:50:56 +0200

    Añadidas 'Ligas' para todos los videojuegos en 'Categorías'

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -8,6 +8,8 @@ export class DataService {
 
   constructor(private http: HttpClient) { }
 
+  // General methods
+
   getLeagues() {
     return this.http.get('https://rjk-backend-hwb4bjfhexaybagg.spaincentral-01.azurewebsites.net/api/leagues')
   }
@@ -24,12 +26,32 @@ export class DataService {
     return this.http.get('https://api.pandascore.co/matches?token=rmhig-Fuz23S9tfDT14uoycApeyVynxJLv2Ljazjz3nYGTT7S4s')
   }
 
+  // Game-specific methods
+
+    // League of Legends
+
+  getLolLeagues() {
+    return this.http.get('https://api.pandascore.co/lol/leagues?token=rmhig-Fuz23S9tfDT14uoycApeyVynxJLv2Ljazjz3nYGTT7S4s')
+  }
+
   getLolMatches() {
     return this.http.get('https://api.pandascore.co/lol/matches?token=rmhig-Fuz23S9tfDT14uoycApeyVynxJLv2Ljazjz3nYGTT7S4s')
   }
 
+    // Counter-Strike: Global Offensive
+
+  getCSGOLeagues() {
+    return this.http.get('https://api.pandascore.co/csgo/leagues?token=rmhig-Fuz23S9tfDT14uoycApeyVynxJLv2Ljazjz3nYGTT7S4s')
+  }
+
   getCSGOMatches() {
     return this.http.get('https://api.pandascore.co/csgo/matches?token=rmhig-Fuz23S9tfDT14uoycApeyVynxJLv2Ljazjz3nYGTT7S4s')
+  }
+
+    // Dota 2
+
+  getDota2Leagues() {
+    return this.http.get('https://api.pandascore.co/dota2/leagues?token=rmhig-Fuz23S9tfDT14uoycApeyVynxJLv2Ljazjz3nYGTT7S4s')
   }
 
   getDota2Matches() {
--- src/app/sections/categories/categories.component.ts
+++ src/app/sections/categories/categories.component.ts
@@ -10,17 +10,36 @@ import { CommonModule } from '@angular/common';
 })
 export class CategoriesComponent {
   constructor(private data: DataService) {
+    this.getLolLeagues();
     this.getLolMatches();
+    this.getCSGOLeagues();
     this.getCSGOMatches();
+    this.getDota2Leagues();
     this.getDota2Matches();
   }
 
   currentViewLoL: string = '';
   currentViewCSGO: string = '';
   currentViewDota2: string = '';
+
+  lolLeagues: any[] = [];
   lolMatches: any[] = [];
+
+  CSGOLeagues: any[] = [];
   CSGOMatches: any[] = [];
+
+  dota2Leagues: any[] = [];
   dota2Matches: any[] = [];
+
+  // LoL
+
+  getLolLeagues() {
+    this.data.getLolLeagues().subscribe((res: any) => {
+      this.lolLeagues = res;
+
+      console.log(this.lolLeagues);
+    });
+  }
 
   getLolMatches() {
     this.data.getLolMatches().subscribe((res: any) => {
@@ -30,11 +49,31 @@ export class CategoriesComponent {
     });
   }
 
+  // CSGO
+
+  getCSGOLeagues() {
+    this.data.getCSGOLeagues().subscribe((res: any) => {
+      this.CSGOLeagues = res;
+
+      console.log(this.CSGOLeagues);
+    });
+  }
+
   getCSGOMatches() {
     this.data.getCSGOMatches().subscribe((res: any) => {
       this.CSGOMatches = res;
 
       console.log(this.CSGOMatches);
+    });
+  }
+
+  // Dota 2
+
+  getDota2Leagues() {
+    this.data.getDota2Leagues().subscribe((res: any) => {
+      this.dota2Leagues = res;
+
+      console.log(this.dota2Leagues);
     });
   }
 
